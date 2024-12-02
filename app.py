44d66a080789aaa8ba33e8f1103ef22a19080c62
Uijin Lee <100072451+dml113@users.noreply.github.com>
2024-12-02 22:08:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis-ro.wrwkvd.ng.0001.apn2.cache.amazonaws.com", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis.wrwkvd.ng.0001.apn2.cache.amazonaws.com", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
